1d72f8bc84eec81281ff104286f3920a7c69f330
bryan <bryllant2003@gmail.com>
2019-03-23 16:55:37 +0100

    fichiers

--- commands/invite.js
+++ commands/invite.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message) => {
-    message.reply("Voici le lien d'invitations du serveur https://discord.gg/46mC9vj");
+    message.reply("Voici le lien pour m'inviter sur ton serveur https://discordapp.com/api/oauth2/authorize?client_id=530517746677579776&permissions=8&scope=bot");
 };
 
 module.exports.help = {
---
+++ commands/server.js
@@ -0,0 +1,9 @@
+
+module.exports.run = async (bot, message) => {
+    message.reply("Voici le lien pour rejoindre le serveur de mon maître ")
+}
+
+module.exports.help = {
+    name: "suggestion"
+  }
+
