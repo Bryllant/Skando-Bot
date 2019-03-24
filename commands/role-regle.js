6937aeeaaf54711343702396ca0fad76fa0cded8
bryan <bryllant2003@gmail.com>
2019-03-24 11:29:03 +0100

    regles

--- commands/role-regle.js
+++ commands/role-regle.js
@@ -4,14 +4,15 @@ module.exports.run = async (bot, message) => {
     if (!message.member.hasPermission('ADMINISTRATOR')) return;
     const sushi = new Discord.RichEmbed()
     .setColor("#008000")
-    .setFooter("Clique sur les réaction pour avoir le rôle", "Chaque role t'ouvrira un salon spécifique")
-    .setFooter("En venant sur le serveur nous considérons que les règles sont lues et acceptés", "Pour avoir accès aux salons, cliquez sur la réaction :white_check_mark:")
-    .setFooter("Role CsGo", "Pour avoir accès au salon cs cliquez sur la réaction csgo")
-    .setFooter("Role fortnite", "Pour avoir accès au salon fortnite, cliquez sur la réaction fortnite")
-    .setFooter("Role notifié", "Pour avoir les notifs du serveur cliquez sur la réaction :bell:")
-    .setFooter("Role minecraft", "Pour avoir accès au salon minecraft, cliquez sur la réaction minecraft")
-    .setFooter("Role rocket league", "Pour avoir accès au salon de rocket league, cliquez sur la réaction rocketleague")
-    .setFooter("Role participant event", "Pour avoir la notif des events cliquez sur la réaction :tada:")
+    .setFooter("Ici tu peux obtenir des rôles")
+    .addField("Clique sur les réaction pour avoir le rôle", "Chaque role t'ouvrira un salon spécifique")
+    .addField("En venant sur le serveur nous considérons que les règles sont lues et acceptés", "Pour avoir accès aux salons, cliquez sur la réaction :white_check_mark:")
+    .addField("Role CsGo", "Pour avoir accès au salon cs cliquez sur la réaction csgo")
+    .addField("Role fortnite", "Pour avoir accès au salon fortnite, cliquez sur la réaction fortnite")
+    .addField("Role notifié", "Pour avoir les notifs du serveur cliquez sur la réaction :bell:")
+    .addField("Role minecraft", "Pour avoir accès au salon minecraft, cliquez sur la réaction minecraft")
+    .addField("Role rocket league", "Pour avoir accès au salon de rocket league, cliquez sur la réaction rocketleague")
+    .addField("Role participant event", "Pour avoir la notif des events cliquez sur la réaction :tada:")
     
     message.channel.send(sushi);
 }
