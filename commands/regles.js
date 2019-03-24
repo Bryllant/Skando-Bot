b08846734e9412576ad07fcb52dc59dae51a1d5c
bryan <bryllant2003@gmail.com>
2019-03-24 11:12:43 +0100

    regles

--- commands/regles.js
+++ commands/regles.js
@@ -41,14 +41,13 @@ module.exports.run = async (bot, message) => {
     const role = new Discord.RichEmbed()
     .setColor("#008000")
     .setFooter("Clique sur les réaction pour avoir le rôle")
-    .setTitle("En venant sur le serveur nous considérons que les règles sont lues et acceptés")
-    .setDescription("Pour avoir accès aux salons, cliquez sur la réaction :white_check_mark:")
+    .setFooter("En venant sur le serveur nous considérons que les règles sont lues et acceptés", "Pour avoir accès aux salons, cliquez sur la réaction :white_check_mark:")
     .setFooter("Role CsGo", "Pour avoir accès au salon cs cliquez sur la réaction csgo")
     .setFooter("Role fortnite", "Pour avoir accès au salon fortnite, cliquez sur la réaction fortnite")
     .setFooter("Role notifié", "Pour avoir les notifs du serveur cliquez sur la réaction :bell:")
     .setFooter("Role minecraft", "Pour avoir accès au salon minecraft, cliquez sur la réaction minecraft")
     .setFooter("Role rocket league", "Pour avoir accès au salon de rocket league, cliquez sur la réaction rocketleague")
-    .setFooter("Role participant event", "Pour avoir la notif des events cliquez sur la réaction :tada: ")
+    .setFooter("Role participant event", "Pour avoir la notif des events cliquez sur la réaction :tada:")
     
     message.channel.send(embed);
     message.channel.send(warn);
