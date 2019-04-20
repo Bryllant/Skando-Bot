97e6c7793bf3b064e1ab1ba74b9dd03ad9887308
bryan <bryllant2003@gmail.com>
2019-04-20 18:23:19 +0200

    ichiers

--- commands/setup-stats.js
+++ commands/setup-stats.js
@@ -2,13 +2,13 @@ module.exports.run = async (bot, message, member) => {
   await message.guild.createChannel('Stats du serveur', 'category')
      let setup = bot.channels.find(c => c.name === "Stats du serveur")
     
-    await message.guild.createChannel("Membres", "text").then(channel => channel.setParent(setup))
+    await message.guild.createChannel("Membres", "voice").then(channel => channel.setParent(setup))
 
-    await message.guild.createChannel("Bots", "text").then(channel => channel.setParent(setup))
+    await message.guild.createChannel("Bots", "voice").then(channel => channel.setParent(setup))
     
-    await message.guild.createChannel("Connectés", "text").then(channel => channel.setParent(setup))
+    await message.guild.createChannel("Connectés", "voice").then(channel => channel.setParent(setup))
     
-    await message.guild.createChannel("Channels", "text").then(channel => channel.setParent(setup))
+    await message.guild.createChannel("Channels", "voice").then(channel => channel.setParent(setup))
 }
 
     module.exports.help = {
