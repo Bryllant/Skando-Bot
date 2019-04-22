40a8164d7700a6d579995feebd96dc846b09bc45
bryan <bryllant2003@gmail.com>
2019-04-22 23:00:31 +0200

    fichier

--- commands/fortnite-stats.js
+++ commands/fortnite-stats.js
@@ -11,6 +11,10 @@ module.exports.run = async (bot, message, args) => {
 
     fortnite.user(username, plateform).then(data => {
         console.log(data);
+    }).catch(err => {
+      console.log(err)
+      message.channel.send("Une erreur est survenue");
+    })
         var stats = data.stats.lifetime
 
 
@@ -38,61 +42,6 @@ module.exports.run = async (bot, message, args) => {
         //.addField("% de top 1", winper, true)
         .addField("k/d", kd, true)
         message.channel.send(embed)
-
-
-/*
-let embed = new Discord.RichEmbed()
-        .setTitle(username)
-        .setColor("#008000")
-        .setDescription("Stats fortnite")
-        .setThumbnail(bot.user.displayAvatarURL)
-        .addField("Top 3", data.stats.lifetime[1]["Top 3s"], true)
-        .addField("Top 5", data.stats.lifetime[0]["Top 5s"], true)
-        .addField("Top 1", data.stats.lifetime[8]["Wins"], true)
-        .addField("Victoire/Défaite", data.stats.lifetime[9]["Win%"], true)
-        .addField("Kill", data.stats.lifetime[10]["Kills"], true)
-        .addField("K/D", data.stats.lifetime[11]["K/d"], true);
-        message.channel.send(embed)
-    });
-    */
-//AVEC UN COLLECTEUR
- /*
-  const filter = m => m.author.id === message.author.id;
-  message.reply("Choissisez un utilisateur... La commande expire dans 10 secondes...").then(q => q.delete(15000))
-  message.channel.awaitMessages(filter, {
-    max: 1,
-    time: 10000
-  }).then(collected => {
-    collected.delete(15000);
-    if (collected.first().content === 'annule') {
-      return message.reply("Commande annulé");
-    }
-
-    let username = collected.first().content;
-
-    let data = fortnite.user(username, 'pc').then(data => {
-      //console.log(data.stats.lifetime)
-      let embed = new Discord.RichEmbed()
-        .setTitle(username)
-        .setColor("#008000")
-        .setDescription("Stats fortnite")
-        .setThumbnail(bot.user.displayAvatarURL)
-        .addField("Top 3", data.stats.lifetime[1]["Top 3s"], true)
-        .addField("Top 5", data.stats.lifetime[0]["Top 5s"], true)
-        .addField("Top 1", data.stats.lifetime[8]["Wins"], true)
-        .addField("Victoire/Défaite", data.stats.lifetime[9]["Win%"], true)
-        .addField("Kill", data.stats.lifetime[10]["Kills"], true)
-        .addField("K/D", data.stats.lifetime[11]["K/d"], true);
-      return message.channel.send(embed);
-    }).catch(err => {
-      message.reply("Utilisateur impossible a trouver").then(r => r.delete(5000));
-    });
-  }).catch(err => {
-    message.reply("Annulé...").then(r => r.delete(5000));
-    console.log("Temps d'attente dépassé await détruit");
-  });
-  */
-});
 }
 
 
