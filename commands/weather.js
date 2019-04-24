2f35df3269252405922c7860b92eaff3f074cf6d
bryan <bryllant2003@gmail.com>
2019-04-24 23:11:22 +0200

    fichiers

--- commands/weather.js
+++ commands/weather.js
@@ -1,9 +1,9 @@
 const weather = require("weather-js")
-
+const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     const lieu = args.join(" ");
     if (!lieu) return message.channel.send(":x: Veuillez insérer le lieu");
-    //if (lieu === "undefined") return message.channel.send(":x: Veuillez insérer un lieu valide");
+    if (lieu === "undefined") return message.channel.send(":x: Veuillez insérer un lieu valide");
     weather.find({search: lieu, degreeType: 'C'}, function(err, result) {
         if(err) console.log(err);
 
