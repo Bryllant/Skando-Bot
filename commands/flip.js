1ff1887f79cfafc7e9532b0411a4e6bf11f914a4
bryan <bryllant2003@gmail.com>
2019-04-25 16:24:17 +0200

    fichiers

--- commands/flip.js
+++ commands/flip.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message) => {
     message.channel.send("Pile ou face?").then(async (m) => {
         const tombe = Math.floor(Math.random() * 2);
+        var resultat = "niet"
         if (tombe = 1){
             resultat = "pile"
         }
@@ -17,7 +18,7 @@ module.exports.run = async (bot, message) => {
 
         collector.on("collect", async (collected) => {
             if (collected.content.toLowerCase() === "pile") {
-                if (resulat === "pile") {
+                if (resultat === "pile") {
                     message.channel.send("Vous avez gagné");
                 } else {
                     message.channel.send("Vous avez perdu");
@@ -25,7 +26,7 @@ module.exports.run = async (bot, message) => {
             }
 
             if (collected.content.toLowerCase() === "face") {
-                if (resulat === "face") {
+                if (resultat === "face") {
                     message.channel.send("Vous avez gagné");
                 } else {
                     message.channel.send("Vous avez perdu");
