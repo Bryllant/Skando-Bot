0325d1833f06e5c23c9584917b658333eda19ceb
bryan <bryllant2003@gmail.com>
2019-04-27 12:02:33 +0200

    fichiers

--- commands/weather.js
+++ commands/weather.js
@@ -13,14 +13,15 @@ module.exports.run = async (bot, message, args) => {
 
         const ville = new Discord.RichEmbed()
         //.setDescription(`**${current.skytext}**`)
-        //.setAuthor(`Ville de ${current.observationpoint}`)
-        //.setThubnail(current.imageURL)
+        .setAuthor(`Ville de ${current.observationpoint}`)
+        .setThubnail(current.imageUrl)
         .setColor("RANDOM")
         .addField("Température", `${current.temperature} degrés`, true)
         .addField("Ressenti", `${current.feelslike}`, true)
-        .addField("Vent", current.winddisplay, true)
+        .addField("Vent", current.windspeed, true)
         .addField("Humidité", `${current.humidity} %`, true)
         .addField("Fuseau", `UTC ${location.timezone}`, true)
+        .addField("Temps", `**${current.skytext}**`, true)
 
 
         message.channel.send(ville)
