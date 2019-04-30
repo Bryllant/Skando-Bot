eb347bfbd1e6b810a9ffa8acbf865c23210eb9b9
bryan <bryllant2003@gmail.com>
2019-04-30 23:50:52 +0200

    fichiers

---
+++ commands/removerole.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  //if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("Vous n'avez pas les permissions nécessaires")
+
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if(!rMember) return message.reply("Impossible de trouver l'utilisateur");
+  let role = args.join(" ").slice(22);
+  if(!role) return message.reply("Spécifiez un rôle");
+  let gRole = message.guild.roles.find(`name`, role);
+  if(!gRole) return message.reply("Je ne peux pas trouver ce rôle");
+
+  if(!rMember.roles.has(gRole.id)) return message.reply("L'utilisateur ne possède pas ce rôle");
+  await(rMember.removeRole(gRole.id));
+
+  try{
+    await rMember.send(`Rip tu a perdu ce rôle ${gRole.name}`)
+  }catch(e){
+    message.channel.send(`Rôle enlevé avec succès!`)
+  }
+}
+
+module.exports.help = {
+  name: "removerole"
+}
