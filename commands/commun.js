efbf78f8f80e73b9260d33baf4e74e0e914e16ec
bryan <bryllant2003@gmail.com>
2019-07-04 22:55:20 +0200

    fichiers

---
+++ commands/commun.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message) => {
+    var number = 0
+    var listes = "" 
+    client.guilds.map(r =>{
+    const list = client.guilds.get(r.id);
+    list.members.map(member => (member.user.id ==message.author.id? number++ : number = number)) 
+    list.members.map(member => (member.user.id ==message.author.id? listes= listes+" | `" +list.name+"`" : listes = listes)) 
+    }) 
+        var embed = new Discord.RichEmbed()
+        .setTimestamp() 
+        .setTitle("Nous possédons " +number+" serveur" +(number == 1 ? "" : "s")+" en commun.") 
+        .addField("📃 __Liste__" , listes+"." ) 
+        .setColor("084c99") 
+     
+        message.channel.send(embed) 
+}
+
+    module.exports.help = {
+    name: "commun"
+  }
