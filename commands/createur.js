45319380c5d610e5d865f6b3b0436c7a1408951a
bryan <bryllant2003@gmail.com>
2020-11-07 17:03:27 +0100

    fichiers

--- commands/createur.js
+++ commands/createur.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (bot, message) => {
-    message.channel.sendMessage("Mon créateur est <@468342061230456833> fait %server pour rejoindre son serveur");
+    message.channel.sendMessage("Mon créateur est <@468342061230456833>");
 };
 
 module.exports.help = {
